7fb983b19088640e8fc220478e628ab6d672cc8a
Rutwik Deshmukh <rutwikdesh28@gmail.com>
2023-08-25 18:01:00 +0530

    first commit

---
+++ src/main/java/com/bookstore/bookhaven/BookhavenApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore.bookhaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookhavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookhavenApplication.class, args);
+	}
+
+}
